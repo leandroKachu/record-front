847f6a60ed9a8dc1e377c5792c26fcc62cbca42c
leandroKachu <lesilvajunior@hotmail.com>
2023-03-23 13:56:23 -0700

    add more two components and configurations

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Signin from '@/components/Signin.vue'
+import Signup from '@/components/Signup.vue'
+
 Vue.use(Router)
 export default new Router({
   mode: 'history',
@@ -9,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Signin',
       component: Signin
+    },
+    {
+      path: '/signup',
+      name: 'Signup',
+      component: Signup
     }
   ]
 })
